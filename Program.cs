23d38b662ea7d3cac25ce3997644a9fb090bd8fa
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-04 20:29:40 -0300

    teminada

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+decimal capital, taxa, meses, juros, montante;
+
+Console.WriteLine("juros simples");
+
+Console.Write("capital:(R$):");
+capital= Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("taxa:(t):");
+taxa= Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Tempo [t] (meses)......: ");
+meses = Convert.ToDecimal(Console.ReadLine());
+
+
+juros = capital * (taxa / 100) * meses;
+montante = capital + juros;
+
+
+Console.WriteLine($"Juros.....: {juros:C2}");
+Console.WriteLine($"Montante..: {montante:C2}");
